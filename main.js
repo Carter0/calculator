54dba65a269beb8f14c343aebd7b49a919780a73
Carter Weinberg <weinbergcarter@gmail.com>
2026-05-06 16:29:01 -0400

    Basic logic is working

--- main.js
+++ main.js
@@ -26,24 +26,25 @@ let variable1 = "";
 let variable2 = "";
 
 function operate(operator, var1, var2) {
-    // !Number return true if the var is an invalid number
-    if (!Number(var1) || !Number(var2)) {
-        return "Invalid variables";
-    }
+    const parsedVar1 = Number(var1);
+    const parsedVar2 = Number(var2);
 
+    if (Number.isNaN(parsedVar1) || Number.isNaN(parsedVar2)) {
+        return "Invalid variables. Press Clear.";
+    }
 
     switch (operator) {
         case "+":
-            return add(var1, var2);
+            return add(parsedVar1, parsedVar2);
             break;
         case "-":
-            return subtract(var1, var2);
+            return subtract(parsedVar1, parsedVar2);
             break;
         case "*":
-            return multiply(var1, var2);
+            return multiply(parsedVar1, parsedVar2);
             break;
         case "/":
-            return divide(var1, var2);
+            return divide(parsedVar1, parsedVar2);
             break;
         default:
             return "Invalid operator";
@@ -57,6 +58,8 @@ function operate(operator, var1, var2) {
 const digits = document.querySelectorAll(".digits button");
 const operators = document.querySelectorAll(".operators button");
 const display = document.querySelector("#display");
+const equalsButton = document.querySelector("#equalsButton");
+const clearButton = document.querySelector("#clearButton");
 
 let wasOperatorAssigned = false;
 
@@ -83,4 +86,21 @@ operators.forEach((operatorButton) => {
         display.textContent = `${variable1} ${operator}`;
         wasOperatorAssigned = true;
     });
+});
+
+equalsButton.addEventListener("click", (event) => {
+    const output = operate(operator, variable1, variable2);
+    display.textContent = output;
+    variable1 = output;
+    operator = "";
+    variable2 = "";
+    wasOperatorAssigned = false;
+});
+
+clearButton.addEventListener("click", (event) => {
+    display.textContent = "Enter a number followed by an operator then another number";
+    variable1 = "";
+    operator = "";
+    variable2 = "";
+    wasOperatorAssigned = false;
 });
