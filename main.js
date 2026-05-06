e937d90c631927800fd1576720e39f6efec02427
Carter Weinberg <weinbergcarter@gmail.com>
2026-05-06 15:12:48 -0400

    Adding the rest of the calculator logic

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function subtract(a, b) {
     return a - b;
 }
 
-function multiple(a, b) {
+function multiply(a, b) {
     return a * b;
 }
 
@@ -15,5 +15,34 @@ function divide(a, b) {
         return "Cannot divide by 0";
     } else {
         return a / b;
+    }
+}
+
+let operator = "";
+let variable1 = "";
+let variable2 = "";
+
+function operate(operator, var1, var2) {
+    // !Number return true if the var is an invalid number
+    if (!Number(var1) || !Number(var2)) {
+        return "Invalid variables";
+    }
+
+
+    switch (operator) {
+        case "+":
+            return add(var1, var2);
+            break;
+        case "-":
+            return subtract(var1, var2);
+            break;
+        case "*":
+            return multiply(var1, var2);
+            break;
+        case "/":
+            return divide(var1, var2);
+            break;
+        default:
+            return "Invalid operator";
     }
 }
